3a17a7697a9b28c0bf664dd195496bfc0278a674
Emma Sun <emma.sun@thoughtworks.com>
2022-04-22 17:00:40 +0800

    Implement id for menuItem

--- src/main/java/domain/MenuItem.java
+++ src/main/java/domain/MenuItem.java
@@ -1,15 +1,24 @@
 package domain;
 
+import java.util.UUID;
+
 public class MenuItem {
     private String name;
     private double price;
+    private final UUID id;
+
+    public MenuItem() {
+        this.id = UUID.randomUUID();
+    }
 
     public MenuItem(String name, double price) {
+        this();
         this.name = name;
         this.price = price;
     }
 
     public MenuItem(String name) {
+        this();
         this.name = name;
     }
 
@@ -19,5 +28,9 @@ public class MenuItem {
 
     public double getPrice() {
         return price;
+    }
+
+    public UUID id() {
+        return id;
     }
 }
